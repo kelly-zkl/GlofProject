2e00a180cbf74d8ebacc865b83a9579cb597a7f4
kelly <1457242108@qq.com>
2018-05-21 19:29:00 +0800

    修改个人信息的标签，球队动态（未完成）

--- pages/team/myTeam/myTeam.js
+++ pages/team/myTeam/myTeam.js
@@ -10,7 +10,12 @@ Page({
     activeIndex: 0,
     sliderOffset: 0,
     sliderLeft: 0,
-    showPopup: false
+    showPopup: false,
+    showReply: false,
+    showDelete: false,
+    dynamics: [],
+    page: 1,
+    size: 10
   },
   onLoad: function (options) {
     var that = this;
@@ -31,12 +36,57 @@ Page({
    */
   onShow: function () {
     this.getGroupDetail();
+    this.getDynamics();
   },
   tabClick: function (e) {
     this.setData({
       sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id
     });
+  },
+  //获取动态列表
+  getDynamics: function (e) {
+    var that = this;
+    http.postRequest({
+      url: "userPost/homePage",
+      params: {
+        belongId: that.data.groupId, page: that.data.page, size: that.data.size,
+        belongType: "group", uid: app.globalData.userInfo.id
+      },
+      msg: "加载中....",
+      success: res => {
+        wx.showToast({ title: '加载成功', icon: 'info', duration: 1500 });
+        this.setData({
+          dynamics: res.data.content
+        })
+      }
+    }, true);
+  },
+  //跳转到发动态页面
+  gotoPost: function () {
+    wx.navigateTo({
+      url: '/pages/userMsg/sendDynamic/sendDynamic?type=user',
+    })
+  },
+  //跳转到个人主页
+  gotoPer: function (e) {
+    if (e.currentTarget.id != app.globalData.userInfo.id) {//本人的动态
+      wx.navigateTo({
+        url: '/pages/userMsg/personalPage/personalPage?tab=0&id=' + e.currentTarget.id,
+      })
+    }
+  },
+  //跳转到比赛详情页
+  gotoGame: function (e) {
+    wx.navigateTo({
+      url: '/pages/game/gameDetail/gameDetail?id=' + e.currentTarget.id,
+    })
+  },
+  //跳转到球队主页
+  gotoTeam: function (e) {
+    wx.navigateTo({
+      url: '/pages/team/teamDetail/teamDetail?id=' + e.currentTarget.id,
+    })
   },
   //球队管理
   togglePopup() {
@@ -170,5 +220,135 @@ Page({
         }
       }
     })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: e.currentTarget.dataset.urls // 需要预览的图片http链接列表
+    })
+  },
+  //删除动态
+  deleteDyna: function (e) {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除该动态？',
+      success: function (res) {
+        if (res.confirm) {
+          http.postRequest({
+            url: "userPost/delete",
+            params: {
+              id: e.currentTarget.id, uid: app.globalData.userInfo.id
+            },
+            msg: "操作中...",
+            success: res => {
+              that.getDynamics();
+              wx.showToast({ title: '删除成功', icon: 'info', duration: 1500 })
+            }
+          }, true);
+        } else if (res.cancel) {
+        }
+      }
+    })
+  },
+  //删除回复
+  deleteReply: function (value) {
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除该回复？',
+      success: function (res) {
+        if (res.confirm) {
+          http.postRequest({
+            url: "replyPost/delete",
+            params: {
+              replyPostId: that.data.beReplyPostId, uid: app.globalData.userInfo.id
+            },
+            msg: "操作中...",
+            success: res => {
+              that.getDynamics();
+              wx.showToast({ title: '删除成功', icon: 'info', duration: 1500 })
+            }
+          }, true);
+        } else if (res.cancel) {
+        }
+      }
+    })
+  },
+  toggleDelete() {
+    this.setData({
+      showDelete: !this.data.showDelete
+    });
+  },
+  deleteChange: function (e) {
+    var that = this;
+    var id = e.currentTarget.id;
+    if (id == 1) {//删除评论
+      that.deleteReply();
+    }
+    that.setData({
+      showDelete: false
+    });
+  },
+  //创建回复
+  createReply: function (e) {
+    var that = this;
+    if (e.detail.value.length > 0) {
+      http.postRequest({
+        url: "replyPost/create",
+        params: {
+          userPostId: that.data.userPostId, uid: app.globalData.userInfo.id, content: e.detail.value,
+          beReplyPostId: that.data.beReplyPostId
+        },
+        msg: "操作中...",
+        success: res => {
+          that.getDynamics();
+        }
+      }, true);
+    }
+    that.setData({
+      showReply: false
+    });
+  },
+  toggleReply() {
+    this.setData({
+      showReply: !this.data.showReply
+    });
+  },
+  pressReply: function (e) {
+    console.log(e);
+    if (e.currentTarget.dataset.replyId) {//有评论id
+      if (e.currentTarget.dataset.uid == app.globalData.userInfo.id) {//本人的动态
+        this.setData({
+          showDelete: true,
+          userPostId: e.currentTarget.id,
+          beReplyPostId: e.currentTarget.dataset.replyId
+        });
+      } else {//自己的评论可删除
+        this.setData({
+          showReply: true,
+          userPostId: e.currentTarget.id,
+          beReplyPostId: e.currentTarget.dataset.replyId
+        });
+      }
+    } else {//没有评论的时候，只能评论
+      this.setData({
+        showReply: true,
+        userPostId: e.currentTarget.id,
+        beReplyPostId: e.currentTarget.dataset.replyId
+      });
+    }
+  },
+  //点赞
+  userVote: function (e) {
+    http.postRequest({
+      url: "userPost/vote",
+      params: {
+        bbsId: e.currentTarget.id, uid: app.globalData.userInfo.id, voted: e.currentTarget.dataset.voted
+      },
+      success: res => {
+        this.getDynamics();
+      }
+    }, false);
   }
 });
